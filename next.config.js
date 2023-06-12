da6c8827677622c7bf116a79d0694f50542cc868
Micha de Vries <micha@devrie.sh>
2023-06-12 22:31:13 +0200

    Move to ESM module type project

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-    });
-    return config;
-  },
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            bufferutil: 'commonjs bufferutil',
+        });
+        return config;
+    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
