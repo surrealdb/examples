fe858bffc13cec1a8a1e08f7dc9af639bae85b92
Micha de Vries <micha@devrie.sh>
2023-06-13 15:44:22 +0200

    Edit note inside note

--- src/components/sticky-editor.tsx
+++ src/components/sticky-editor.tsx
@@ -10,6 +10,7 @@ import React, {
     ReactNode,
     createRef,
     useCallback,
+    useEffect,
     useState,
 } from 'react';
 import { animated, config, useTransition } from 'react-spring';
@@ -43,29 +44,13 @@ export function StickyEditor({
     color: StickyColor;
     children: ReactNode;
 }) {
-    const ref = createRef<HTMLTextAreaElement>();
     const [open, setOpen] = useState(false);
-
-    // The enter key is used to submit the sticky editor
-    // So we need to listen for that, but only when the shift key is not pressed
-    // When shift is pressed, we still want to go for a newline :)
-    const onKeyDown = useCallback(
-        (e: KeyboardEvent<HTMLTextAreaElement>) => {
-            if (e.keyCode == 13 && e.shiftKey == false) {
-                // Detected! Prevent a newline from being created
-                e.preventDefault();
-
-                // Check for fault input
-                if (!ref.current)
-                    return alert('Internal error (input not mounted)');
-                if (!ref.current.value.trim())
-                    return alert('The sticky is empty!');
-
-                onSubmit({ color, content: ref.current.value.trim() });
-                setOpen(false);
-            }
+    const submit = useCallback(
+        (content: string) => {
+            onSubmit({ color, content });
+            setOpen(false);
         },
-        [ref, color, onSubmit]
+        [color, onSubmit]
     );
 
     const transitions = useTransition(open, {
@@ -107,12 +92,9 @@ export function StickyEditor({
                                     </Dialog.Close>
                                 </div>
 
-                                <textarea
-                                    ref={ref}
-                                    placeholder="Enter the content for your sticky here"
-                                    className="w-full flex-grow bg-transparent text-2xl placeholder-gray-600 outline-none"
-                                    onKeyDown={onKeyDown}
-                                    defaultValue={initialValue}
+                                <Textarea
+                                    onSubmit={submit}
+                                    initialValue={initialValue}
                                 />
                             </animated.div>
                         </Dialog.Content>
@@ -120,5 +102,65 @@ export function StickyEditor({
                 ) : null
             )}
         </Dialog.Root>
+    );
+}
+
+export function Textarea({
+    className,
+    onSubmit,
+    initialValue,
+    editing,
+    setEditing,
+}: {
+    className?: string;
+    onSubmit: (content: string) => unknown;
+    initialValue?: string;
+    editing?: boolean;
+    setEditing?: (editing: boolean) => unknown;
+}) {
+    const ref = createRef<HTMLTextAreaElement>();
+
+    useEffect(() => {
+        if (editing && ref.current) {
+            ref.current.selectionStart = ref.current.value.length;
+            ref.current.selectionEnd = ref.current.value.length;
+            ref.current.focus();
+        }
+    });
+
+    // The enter key is used to submit the sticky editor
+    // So we need to listen for that, but only when the shift key is not pressed
+    // When shift is pressed, we still want to go for a newline :)
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent<HTMLTextAreaElement>) => {
+            if (e.key === 'Enter' && e.shiftKey === false) {
+                // Detected! Prevent a newline from being created
+                e.preventDefault();
+
+                // Check for fault input
+                if (!ref.current)
+                    return alert('Internal error (input not mounted)');
+                if (!ref.current.value.trim())
+                    return alert('The sticky is empty!');
+
+                onSubmit(ref.current.value.trim());
+            } else if (e.key === 'Escape') {
+                setEditing?.(false);
+            }
+        },
+        [ref, onSubmit, setEditing]
+    );
+
+    return (
+        <textarea
+            ref={ref}
+            placeholder="Enter the content for your sticky here"
+            className={cn(
+                'w-full flex-grow bg-transparent text-2xl text-dark placeholder-gray-600 outline-none',
+                className
+            )}
+            onKeyDown={onKeyDown}
+            defaultValue={initialValue}
+        />
     );
 }
--- src/components/sticky.tsx
+++ src/components/sticky.tsx
@@ -4,8 +4,8 @@ import { useDeleteSticky, useUpdateSticky } from '@/lib/hooks';
 import { cn } from '@/lib/utils';
 import { cva } from 'class-variance-authority';
 import { X } from 'lucide-react';
-import React from 'react';
-import { StickyEditor } from './sticky-editor';
+import React, { createRef, useCallback, useEffect, useState } from 'react';
+import { Textarea } from './sticky-editor';
 
 const style = cva('', {
     variants: {
@@ -32,32 +32,61 @@ export function Sticky({
     color: StickyColor;
     content: string;
 }) {
+    const ref = createRef<HTMLDivElement>();
+    const [editing, setEditing] = useState(false);
     const { trigger: deleteSticky } = useDeleteSticky(id);
     const { trigger: updateSticky } = useUpdateSticky(id);
 
+    const submit = useCallback(
+        (content: string) => {
+            updateSticky({ color, content });
+            setEditing(false);
+        },
+        [color, updateSticky]
+    );
+
+    useEffect(() => {
+        const handler = (event: globalThis.MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                setEditing(false);
+            }
+        };
+        window.addEventListener('mousedown', handler);
+        return () => window.removeEventListener('mousedown', handler);
+    }, [ref]);
+
     return (
-        <div className="relative transition-transform hover:scale-105">
+        <div
+            className={cn(
+                'relative rounded-4xl transition-transform hover:scale-105 hover:shadow-lg active:scale-100',
+                editing && 'scale-105 shadow-lg'
+            )}
+            ref={ref}
+        >
             <button
                 className="absolute right-0 top-0 z-10 m-4 border-none bg-transparent p-0 text-white outline-none"
                 onClick={() => deleteSticky()}
             >
                 <X size={30} />
             </button>
-            <StickyEditor
-                title="Edit sticky"
-                onSubmit={updateSticky}
-                color={color}
-                initialValue={content}
+            <button
+                className={cn(
+                    'relative flex aspect-square w-full cursor-pointer flex-col items-start rounded-4xl border-none px-10 py-14 text-left text-2xl text-dark outline-none',
+                    style({ color })
+                )}
+                onClick={() => setEditing(true)}
             >
-                <div
-                    className={cn(
-                        'relative aspect-square w-full cursor-pointer rounded-4xl px-10 py-14 text-2xl text-dark',
-                        style({ color })
-                    )}
-                >
-                    {content}
-                </div>
-            </StickyEditor>
+                {editing ? (
+                    <Textarea
+                        initialValue={content}
+                        onSubmit={submit}
+                        editing={editing}
+                        setEditing={setEditing}
+                    />
+                ) : (
+                    <p>{content}</p>
+                )}
+            </button>
         </div>
     );
 }
